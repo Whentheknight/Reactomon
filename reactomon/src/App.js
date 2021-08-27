83f398954ca935fa0c8556dbf80e0419eb1a7717
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-08-27 17:59:41 +0200

    Add theme changer icon, it spins the div inside the PokemonList.js, want to add music to the site and improve spin in the future

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -3,36 +3,43 @@ import PokemonList from "./components/PokemonList";
 import TypeList from "./components/TypeList";
 import PokemonDetail from "./components/PokemonDetail";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ThemeContext from "./context/ThemeContext";
+import { useState } from "react";
+import ThemeToggler from "./components/ThemeToggler";
 
 function App() {
+  const themeHook = useState("noSpin");
   return (
-    <Router>
-      <div className="mainDiv">
-        <div className="navContainer">
-          <Navbar />
+    <ThemeContext.Provider value={themeHook}>
+      <Router>
+        <div className="mainDiv">
+          <ThemeToggler />
+          <div className="navContainer">
+            <Navbar />
+          </div>
+          <Switch>
+            <Route
+              path="/pokemons"
+              render={(props) => <PokemonList {...props} />}
+              exact
+            />
+
+            <Route
+              path="/types"
+              render={(props) => <TypeList {...props} />}
+              exact
+            />
+
+            <Route
+              path="/pokemon/:id"
+              render={(props) => <PokemonDetail {...props} />}
+              exact
+            />
+          </Switch>
         </div>
-        <Switch>
-          <Route
-            path="/pokemons"
-            render={(props) => <PokemonList {...props} />}
-            exact
-          />
-
-          <Route
-            path="/types"
-            render={(props) => <TypeList {...props} />}
-            exact
-          />
-
-          <Route
-            path="/pokemon/:id"
-            render={(props) => <PokemonDetail {...props} />}
-            exact
-          />
-        </Switch>
-      </div>
-      <br></br>
-    </Router>
+        <br></br>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
--- reactomon/src/components/PokemonList.js
+++ reactomon/src/components/PokemonList.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-
+import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AppTheme from "../Spin";
+import ThemeContext from "../context/ThemeContext";
 
 function PokemonList() {
-  const [pokemons, setPokemons] = useState([]);
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=16');
-  
+  const theme = useContext(ThemeContext)[0];
+  const currentTheme = AppTheme[theme];
 
+  const [pokemons, setPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=16"
+  );
 
   const getPokemons = async () => {
-    const result = await fetch(loadMore)
-    const data = await result.json()
+    const result = await fetch(loadMore);
+    const data = await result.json();
 
-    setLoadMore(data.next)
+    setLoadMore(data.next);
 
-    function createPokemonObject(results){
-      results.forEach(async pokemon => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data = await res.json()
-        setPokemons(currentList => [...currentList, data])
-        pokemons.sort((a, b)=> a.id - b.id)
-      })
-      
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setPokemons((currentList) => [...currentList, data]);
+        pokemons.sort((a, b) => a.id - b.id);
+      });
     }
-    createPokemonObject(data.results)
+    createPokemonObject(data.results);
   };
 
   useEffect(() => {
@@ -32,24 +38,59 @@ function PokemonList() {
   }, []);
 
   return (
-    <div className="names">
-      {pokemons.map((pokemon, index) => (
-        <Link to={`/pokemon/${index + 1}`}>
-        <div className="pokemonName" key={`${index + 1}`}>
-          <img src={pokemon.sprites.other.dream_world.front_default}></img>
-          <br></br>
-          <Paragh><p>{pokemon.name}</p></Paragh>
+    <div>
+      {theme === "spin" ? (
+        <Diver className="names">
+          {pokemons.map((pokemon, index) => (
+            <Link to={`/pokemon/${index + 1}`}>
+              <div className="pokemonName" key={`${index + 1}`}>
+                <img
+                  src={pokemon.sprites.other.dream_world.front_default}
+                ></img>
+                <br></br>
+                <Paragh>
+                  <p>{pokemon.name}</p>
+                </Paragh>
+              </div>
+            </Link>
+          ))}
+        </Diver>
+      ) : (
+        <div className="names">
+          {pokemons.map((pokemon, index) => (
+            <Link to={`/pokemon/${index + 1}`}>
+              <div className="pokemonName" key={`${index + 1}`}>
+                <img
+                  src={pokemon.sprites.other.dream_world.front_default}
+                ></img>
+                <br></br>
+                <Paragh>
+                  <p>{pokemon.name}</p>
+                </Paragh>
+              </div>
+            </Link>
+          ))}
         </div>
-        </Link>
-      ))}
+      )}
     </div>
-    
   );
 }
 
 const Paragh = styled.p`
-font-size: 1rem;
-margin-top: -2rem;
+  font-size: 1rem;
+  margin-top: -2rem;
+`;
+
+const spinning = keyframes`
+0% {
+  transform: rotate(0) translate3D(-50%, -50%, 0);
+}
+100% {
+  transform: rotate(360deg) translate3D(-50%, -50%, 0);
+}`;
+
+const Diver = styled.div`
+  animation: ${spinning} 5s infinite;
 `;
 
 export default PokemonList;
